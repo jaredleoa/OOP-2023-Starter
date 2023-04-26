ae42bff75316a1ddfdf0e77dcbacd8e4caa94b54
Jared <jaredleo@icloud.com>
2023-04-26 17:48:42 +0100

    Printing sonnet to console and screen, as well as writeSonnet

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -3,12 +3,14 @@ package ie.tudublin;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import processing.data.TableRow;
 
 public class DANI extends PApplet {
 
 	
 
 	private String[] words;
+	private String[] sonnet;
 
 void loadFile()
 {
@@ -17,6 +19,20 @@ void loadFile()
     sonnet = sonnet.replaceAll("[^\\w\\s]",""); // Remove punctuation characters
     sonnet = sonnet.toLowerCase(); // Convert the string to lower case 
     words = sonnet.split(" "); // Split the string into an array of words
+	
+}
+
+public void printSonnetToConsole() {
+	System.out.println(sonnet);
+}
+
+public void printSonnetToScreen() {
+	background(0);
+	fill(255);
+	noStroke();
+	textSize(20);
+	textAlign(CENTER, CENTER);
+	text(sonnet, width/2, height/2);
 }
 
 	public void settings() {
@@ -36,13 +52,33 @@ void loadFile()
 		}
 	}
 
+	public void printModel()
+	{
+		
+	}
+
 
 	
     String[] sonnet;
 
     public String[] writeSonnet()
     {
-        return null;
+        ArrayList<String> sentence = new ArrayList<String>();
+    String currentWord = words[(int)random(words.length)];
+    sentence.add(currentWord);
+
+    for(int i = 0; i < 7; i++) {
+        ArrayList<String> follows = new ArrayList<String>();
+        for(int j = 0; j < words.length - 1; j++) {
+            if(words[j].equals(currentWord)) {
+                follows.add(words[j + 1]);
+            }
+        }
+    }
+
+    String[] sentenceArray = new String[sentence.size()];
+    sentence.toArray(sentenceArray);
+    return sentenceArray;
     }
 
 	public void setup() {
